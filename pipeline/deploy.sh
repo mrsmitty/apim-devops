9f47d4d94b285b3918c73d2e8e964ad26376650b
APIM Automation <apim@devops.com>
2021-05-25 06:15:40 +0000

    build

--- pipeline/deploy.sh
+++
@@ -1,9 +0,0 @@
-export DEST_APIM="PWS-SYD-APIM-DEVCICD"
-export API_NAME="PetStoreV3"
-export RESOURCE_GROUP="PWS-SYD-ARG-CICD"
-export TEMPLATE_DIRECTORY="/workspaces/apim-devops/api/PetStoreV3"
-export BUILD_ID="1"
-
-
-
-source apim-template-deploy.sh 
--- pipeline/extract.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export DEST_APIM="PWS-SYD-DEV-ARG-CICD"
-export API_NAME="PetStoreV3"
-export RESOURCE_GROUP="PWS-SYD-ARG-CICD"
-export SOURCE_APIM="PWS-SYD-APIM-CICD"
-export TEMPLATE_DIRECTORY="/workspaces/apim-devops/api/PetStoreV3"
-
-source apim-template-extract.sh
